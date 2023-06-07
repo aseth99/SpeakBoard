c61ba584b8bd847c67291a776ac7e518572d81c8
Aman Seth <99aseth@gmail.com>
2023-06-06 23:37:21 -0600

    Set up Express.js server with MongoDB connection

---
+++ SpeakBoardApp/Server/server.js
@@ -0,0 +1,28 @@
+const express = require('express');
+const cors = require('cors');
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+
+const app = express();
+
+app.use(cors());
+app.use(bodyParser.json());
+
+
+mongoose.connect('mongodb://localhost:27017/speakboard', {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+}).then(() => {
+    console.log('Connected to database');
+}).catch((err) => {
+    console.log('Error connecting to database', err);
+});
+
+app.get('/', (req, res) => {
+    res.send('Hello, world!');
+});
+
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
+    console.log(`Server started on port ${port}`);
+});
